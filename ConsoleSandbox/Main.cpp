3b844e092cdf8d0b6aa7b1da096a5f57d6b5323e
AxLundberg <axel__lundberg@hotmail.com>
2023-05-14 16:57:40 +0200

    Unit tests added

--- ConsoleSandbox/Main.cpp
+++ ConsoleSandbox/Main.cpp
@@ -2,24 +2,24 @@
 
 #include "Core/src/ioc/Container.h"
 
-struct Base
-{
-	virtual int Test() { return 0; }
-	virtual ~Base() = default;
-};
-
-struct Derived : public Base
-{
-	int Test() override { return 1; }
-};
+//struct Base
+//{
+//	virtual int Test() { return 0; }
+//	virtual ~Base() = default;
+//};
+//
+//struct Derived : public Base
+//{
+//	int Test() override { return 1; }
+//};
 
 int main()
 {
 	using namespace CPR;
 
-	IOC::Get().Register<Base>([] {return std::make_shared<Derived>(); });
+	/*IOC::Get().Register<Base>([] {return std::make_shared<Derived>(); });
 
-	std::cout << IOC::Get().Resolve<Base>()->Test() << std::endl;
+	std::cout << IOC::Get().Resolve<Base>()->Test() << std::endl;*/
 
 	return 0;
 }
